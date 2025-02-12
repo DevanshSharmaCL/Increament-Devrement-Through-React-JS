76201a0d3400a399f0ba8456a3b3e5d5d5306ffd
DevanshSharmaCL <devanshsharma70cl@gmail.com>
2025-02-12 17:28:15 +0530

    First React Project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-//Whenever we building an component we name it in CAPITAL Lette
+import React,{useState} from 'react'
 
-import { Fragment } from "react";
-
-function App(){
-    let a = 10;
+const App = () =>{
+    const [count,setcount]=useState(0);
+    
+    const handleClickInc = () => {
+        setcount(count+1);
+    }
+    const handleClickDec = () => {
+        setcount(count-1);
+    }
     return(
-        <Fragment>
-        <h1>JSX stands for JavaScript Syntax Extension , and All this which is written here is JSX</h1>
-        <p>we have to use fragments which are these "<> </>"</p>
-        <p>now,to use JS in here we use this {a} </p>
-        </Fragment>
-        //fragments all u to return multiple elements from react components by allowing you to group a list of children wihout adding any extra nodes 
+        <div>
+            <h1>{count}</h1>
+            <button onClick={handleClickInc}>+</button>
+            <button onClick={handleClickDec}>-</button>
+        </div>
+
     )
 }
 
-export default App;
-
-//2a. Now why are we exporting this
-//2a. Beacuse we want to render all this in "index.js" 
+export default App
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import React from "react";        //we importing react libraries and component from react itself
-import ReactDOM from 'react-dom/client';    //we importing react dom which is rendering the react component in out broswer
-import App from "./App";  //2a.now by importing we can use the component of "App.js" inside "index.js"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));// from createRoot method we are craeting new root , 1. this root will render in "Public"=>"Index.HTML"
-
+const root = ReactDOM.createRoot(document.getElementById('root')); // Corrected
 root.render(
-  //for examples
-  // <h1>hello world </h1>,
-  <React.StrictMode> 
-    <App/>
-  </React.StrictMode>
-)
-
-//"root.render" allows us to render whatever we want to render on the screen 
-//"React.StrictMode" is like a developer mode feature , which help in identifying the potential issue which might come while building app
-//its note cumpulsory to use it , but its a good practice to use it .
-//2a. but to make it work look at the line 11 , <App/> now this is what,which gonna render the mssg from App.js into the Index.js , and u can repeat it as many as time u want , more like reuse
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
